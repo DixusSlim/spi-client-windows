6610672267215a04bea421ed1f472bead8835033
Raymond <Raysta@gmail.com>
2019-04-09 08:47:51 +1000

    Expose class interface for COM

--- SPIClient/Device.cs
+++ SPIClient/Device.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace SPIClient
 {
+    [ClassInterface(ClassInterfaceType.AutoDual)]
     public class DeviceAddressChangedEventArgs : EventArgs
     {
         public string DeviceAddress { get; set; }
